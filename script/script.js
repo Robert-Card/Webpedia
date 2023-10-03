a50819f78643e26fb57d56d164f93dc1b9f92afc
Robert-Card <robertleandroc@gmail.com>
2023-10-03 19:22:15 -0300

    First Commit

---
+++ script/script.js
@@ -0,0 +1,36 @@
+const themeMap = {
+    dark: "light",
+    light: "solar",
+    solar: "dark"
+  };
+  
+  //Salvar preferência de tema localmente
+  const theme = localStorage.getItem('theme')
+    || (tmp = Object.keys(themeMap)[0],
+        localStorage.setItem('theme', tmp),
+        tmp);
+  const bodyClass = document.body.classList;
+  bodyClass.add(theme);
+  
+  function toggleTheme() {
+    const current = localStorage.getItem('theme');
+    const next = themeMap[current];
+  
+    bodyClass.replace(current, next);
+    localStorage.setItem('theme', next);
+  }
+  
+  document.getElementById('themeButton').onclick = toggleTheme;
+
+
+
+
+
+
+
+
+
+
+
+
+ 
